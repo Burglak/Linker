8ce30868c1a6a00a64e7f0ae3a7bc86d8e5b720d
Burglak <you@example.com>
2024-10-18 01:40:37 +0200

    Added 2 models: User, UserStatus

--- src/main/java/com/burglak/linker/model/entity/User.java
+++ src/main/java/com/burglak/linker/model/entity/User.java
@@ -1,4 +1,53 @@
 package com.burglak.linker.model.entity;
 
+import com.burglak.linker.model.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data //create getters and setters
+@Builder //add builder for creating objects
+@AllArgsConstructor //create constructor with all arguments constructor
+@NoArgsConstructor //create no arguments constructor
+@Entity //entity representing a table stored in a database
+@Table(name = "app_user") //name of the table
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto generated id
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    private String phone;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    private String bio;
+
+    @Column(name = "profile_picture_path", nullable = true)
+    private String profilePicturePath;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserRole userRole;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "last_login", nullable = true)
+    private Timestamp lastLogin;
+
 }
---
+++ src/main/java/com/burglak/linker/model/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.burglak.linker.model.enums;
+
+public enum UserRole {
+    USER,
+    ADMIN,
+    SUPPORT
+}
