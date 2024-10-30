ee6418aeddfd41c0e9d0ea445d3dc3fb78a48f6f
burglak <cyprian.schiffer123@gmail.com>
2024-10-30 23:47:00 +0100

    implemented methods from UserDetails, Principal in User entity

--- src/main/java/com/burglak/linker/model/entity/User.java
+++ src/main/java/com/burglak/linker/model/entity/User.java
@@ -7,16 +7,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Principal;
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 
 @Data //create getters and setters
 @Builder //add builder for creating objects
 @AllArgsConstructor //create constructor with all arguments constructor
 @NoArgsConstructor //create no arguments constructor
 @Entity //entity representing a table stored in a database
-@Table(name = "app_user") //name of the table
-public class User {
+@Table(name = "_user") //name of the table
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto generated id
@@ -28,9 +37,10 @@ public class User {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(unique = true)
     private String phone;
 
     @Column(name = "password", nullable = false)
@@ -49,10 +59,58 @@ public class User {
     private Boolean isActive;
 
     @CreationTimestamp
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Timestamp createdAt;
 
-    @Column(name = "last_login", nullable = true)
+    @LastModifiedDate
+    @Column(name = "last_login", insertable = false)
     private Timestamp lastLogin;
 
+    private boolean accountLocked;
+    private boolean enabled;
+
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + userRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    private String fullName() {
+        return firstName + " " + lastName;
+    }
 }
