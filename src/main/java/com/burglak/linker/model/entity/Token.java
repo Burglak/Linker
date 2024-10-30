cdb735adf3aa2b6916242cf43e4ebcad7865bb56
burglak <cyprian.schiffer123@gmail.com>
2024-10-30 23:58:33 +0100

    added: Token

---
+++ src/main/java/com/burglak/linker/model/entity/Token.java
@@ -0,0 +1,39 @@
+package com.burglak.linker.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data //create getters and setters
+@Builder //add builder for creating objects
+@AllArgsConstructor //create constructor with all arguments constructor
+@NoArgsConstructor //create no arguments constructor
+@Entity //entity representing a table stored in a database
+@Table(name = "token") //name of the table
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String token;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+
+    private Timestamp expiresAt;
+
+    private Timestamp validatedAt;
+
+
+}
