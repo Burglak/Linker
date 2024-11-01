9023fd69f67c5780a9938ba5e9b366542a053519
burglak <cyprian.schiffer123@gmail.com>
2024-11-01 23:58:03 +0100

    github streak commit

--- src/main/java/com/burglak/linker/config/SecurityConfig.java
+++ src/main/java/com/burglak/linker/config/SecurityConfig.java
@@ -34,5 +34,6 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider)
                 .addFilter(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
+
     }
 }
