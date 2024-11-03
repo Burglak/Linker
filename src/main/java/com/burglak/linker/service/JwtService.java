a12cd61b36af41d899d5d75cb85e829e8bc75f05
burglak <cyprian.schiffer123@gmail.com>
2024-11-03 23:32:42 +0100

    jwt service comments update

--- src/main/java/com/burglak/linker/service/JwtService.java
+++ src/main/java/com/burglak/linker/service/JwtService.java
@@ -18,19 +18,25 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
+    //jwt expiration time is injected from the application properties
     @Value("${application.security.jwt.expiration}")
     private long jwtExpiration;
+
+    //jwt secret key is injected from the application properties
     @Value("${application.security.jwt.secret-key}")
     private String secretKey;
 
+    //generate a JWT token with additional claims
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    //build the JWT token with given claims, user details, and expiration
     private String generateToken(Map<String, Object> claims, UserDetails userDetails) {
         return buildToken(claims, userDetails, jwtExpiration);
     }
 
+    //build and sign the JWT token
     private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long jwtExpiration) {
         var authorities = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
         return Jwts.builder()
@@ -44,34 +50,40 @@ public class JwtService {
 
     }
 
+    //validate the JWT token against user details
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
+    //check if the jwt token is expired
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    //extract the expiration date from the JWT token
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    //retrieve the signing key from the secret key stored in Base64 format
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-
+    //extract the username (subject) from the JWT token
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    //general method to extract any claim from the JWT token using a claim resolver function
     public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
         final Claims claims = extractAllClaims(token);
         return claimResolver.apply(claims);
     }
 
+    //parse and extract all claims from the JWT token
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parser()
